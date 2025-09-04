afa45b6703413017799e7d8d4cb378a5ff79533b
Sofie <sov.002@edu.zealand.dk>
2025-09-04 23:37:33 +0200

    wf

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-const product = 'Socks'
+
+const app = Vue.createApp()
+Vue.createApp({
+    data()
+    {return{
+                product: 'Socks',
+                description:' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus est pariatur explicabo assumenda saepe delectus doloribus dolore sunt animi sequi non corrupti sint veritatis nisi perspiciatis deserunt incidunt, et in?',
+                image: '.assets/images/socks_green.jpg',
+
+    }
+    }
+}).mount('#app')
